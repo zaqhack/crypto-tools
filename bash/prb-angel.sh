4c89c397a2e802adbbd4287f2d38d916f194c82d
Zack Stone <z@sgt.cc>
2021-11-16 10:33:49 -1000

    Update prb-angel.sh
    Checking for node disconnect is too aggressive (100 vs 30 - set to 30.

--- bash/prb-angel.sh
+++ bash/prb-angel.sh
@@ -72,7 +72,7 @@ function _check_logs {
         # of the process log? If so, restart process
 
         PANIC=$(/usr/bin/docker logs --tail 30 "phala_${1}_1" 2>&1 | grep -i fatal | wc -l)
-        PANIC=$((${PANIC}+$(/usr/bin/docker logs --tail 100 "phala_${1}_1" 2>&1 | grep -i "disconnected from ws" | wc -l) ))
+        PANIC=$((${PANIC}+$(/usr/bin/docker logs --tail 30 "phala_${1}_1" 2>&1 | grep -i "disconnected from ws" | wc -l) ))
         if [ "${PANIC}" -gt 0 ]
         then
                 # Is this run from a cron job?
